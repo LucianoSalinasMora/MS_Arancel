3918ba18c2e5fce2911eea08f8b583b9573beb36
LucianoSalinasMora <luci.salinasm@duocuc.cl>
2026-05-17 23:27:38 -0400

    Add files via upload

---
+++ MS_Arancel/src/test/java/SigueTuCarrera/Arancel/ArancelApplicationTests.java
@@ -0,0 +1,13 @@
+package SigueTuCarrera.Arancel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArancelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
